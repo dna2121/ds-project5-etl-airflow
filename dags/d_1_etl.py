1676010d4f8967beaa03e2fea5091ada1a4df7b7
dna2121 <dnatayawibisono@gmail.com>
2023-11-07 22:06:57 +0700

    ETL process

--- dags/d_1_etl.py
+++ dags/d_1_etl.py
@@ -3,6 +3,7 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.exceptions import AirflowSkipException
+from airflow.models import Connection
 
 from datetime import datetime
 from google.oauth2 import service_account
@@ -13,7 +14,7 @@ import pandas as pd
 import re
 
 
-#module
+# MODULE
 def get_file_list(folder_id):
     credentials = service_account.Credentials.from_service_account_file(
         '/opt/airflow/dags/credentials/service-account.json', scopes=['https://www.googleapis.com/auth/drive.readonly']
@@ -46,21 +47,79 @@ def download_from_fileid(file_id, filename):
             file.write(response.content)
     else:
         print("Failed to download the CSV.")
-#end
+
+def transform_extract_user_agent(items):
+    user_agent = items['UserAgent']
+    browser_regex = r"(\w+)/\d+\.\d+"
+    os_regex = r"\(([^;]+)"
+    # Extract browser and OS information
+    browser_match = re.search(browser_regex, user_agent)
+    os_match = re.search(os_regex, user_agent)
+
+    browser_info = browser_match.group(1) if browser_match else "N/A"
+    os_info = os_match.group(1) if os_match else "N/A"
+
+    new_columns = pd.Series({
+        'browser_info': browser_info,
+        'operating_system': os_info
+    })
+    return new_columns
+# END MODULE
+
 
 #function check
 def fun_check_if_file_exists(**context):
     execution_date = context['execution_date'].strftime('%Y-%m-%d')
     print('check date: ', execution_date)
-    for file in get_file_list('181R-ug68jYWaI9vBttHnHbt7cTQbuZ1s'):
+    for file in get_file_list('1Uv3Y4nlkIElxcjUlCS6ujsuNhGuEAMNM'):
         if execution_date in file['name']:
             return True
     raise AirflowSkipException
 
 #function etl
 def fun_etl(**context):
-    pass
+    # extract
+    data = None
+    execution_date = context['execution_date'].strftime('%Y-%m-%d')
+    print('check date: ', execution_date)
+    for file in get_file_list('1Uv3Y4nlkIElxcjUlCS6ujsuNhGuEAMNM'):
+        if execution_date in file['name']:
+            download_from_fileid(file['id'], file['name'])
+            data = pd.read_csv(file['name'])
+            break
 
+    try:
+        # transform
+        print("Data from Google Drive : ")
+        print(data.head())
+        print(data.info())
+        new_columns = data.apply(transform_extract_user_agent, axis=1)
+        df = pd.concat([data, new_columns], axis=1)
+        df['execution_date'] = execution_date
+        print("After Transform : ")
+        print(df.head())
+        print(df.info())
+    except Exception as e:
+        print('Invalid file.!')
+        print(e)
+
+    # load
+    config = Connection.get_connection_from_secrets("my-postgres")
+    db_params = {
+        "user": config.login,
+        "password": config.password,
+        "host": config.host,
+        "port": config.port,
+        "database": config.schema
+    }
+    engine = create_engine(
+        f'postgresql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["database"]}')
+    schema_name = "public"
+    table_name = "users_visitor_denise"
+    df.to_sql(table_name, engine, schema=schema_name, if_exists='append', index=False)
+    engine.dispose()
+
+# THE DAG
 with DAG(
     dag_id='d_1_etl',
     start_date=datetime(2023, 10, 1),
@@ -68,7 +127,7 @@ with DAG(
 
     #catch up true berarti akan running mengikuti start date nya.
     #catch up false berarti running nya interval terakhir
-    catchup=False,
+    catchup=True,
 
     #supaya run nya maksimal 1 atau brp yg ditentukan
     max_active_runs=1 
